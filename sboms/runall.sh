2b5672ad972680415f74d73c5ed791ddc13ef168
whart222 <whart222@gmail.com>
2024-03-21 09:41:38 -0600

    Adding script to generate sboms

---
+++ sboms/runall.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Generate the SBOMs in this directory
+#
+\rm -f *.json
+. "$HOME/miniconda3/etc/profile.d/conda.sh"
+
+conda create -n generate_sboms -y python=3.10 pipx numpy scipy pytest
+conda activate generate_sboms
+pip install platformdirs
+pip install sbom4python
+pip install cyclonedx-bom
+
+echo "Install example_python_sbom with pip"
+cd ..
+pip install -e .
+cd sboms
+
+# Generate SBOM from environment
+cyclonedx-py environment -o cyclonedx_env_sbom.json
+
+# Generate SBOM from installed package
+sbom4python --module example_python_sbom --sbom cyclonedx --format json -o sbom4python_module_sbom.json
+
+pip uninstall -y example_python_sbom
+
+echo "Install example_python_sbom with poetry"
+cd ..
+rm -f poetry.lock
+poetry install
+cd sboms
+
+# Generate SBOM from poetry configuration
+cd ..
+cyclonedx-py poetry -o sboms/cyclonedx_poetry_sbom.json
+cd sboms
+
+conda deactivate
+conda env remove -n generate_sboms
